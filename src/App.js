e962dbe49e44879c920ab8337e37092f592eb298
Farhan Ahmed <Farhan.ahmed5160@gmail.com>
2020-06-06 20:47:38 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          From Farhan Ahmed
+          <br/>
+          Update by Farhan Ahmed
         </p>
         <a
           className="App-link"
