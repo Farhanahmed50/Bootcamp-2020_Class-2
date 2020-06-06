eb2b43bda48ddf41ae506a8a1ab902ffb6a5c44f
Farhan Ahmed <Farhan.ahmed5160@gmail.com>
2020-06-06 20:50:15 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
           <br/>
-          Update by Farhan Ahmed
+          Update by Farhan Ahmed.
         </p>
         <a
           className="App-link"
